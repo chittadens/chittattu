1472b5dc5bd2efb188fbb8ed28ecdf76cbbe70ce
shajilcbabu <shajilcbabu11@gmail.com>
2022-02-03 15:13:07 +0530

    v1

--- shoppingproject/shoppingproject/settings.py
+++ shoppingproject/shoppingproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@iwp8ne1)*n&7ro!(^=i-w&5ksw0d9c5v1%atm&#7lefcut&od
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
